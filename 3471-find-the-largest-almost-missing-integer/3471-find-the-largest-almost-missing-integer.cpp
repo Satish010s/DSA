2ade11e2970639d6aca71254e43d912d0e68f9ac
AlgoSatish <satishkumaryadav9905@gmail.com>
2026-08-18 20:38:17 +0530

    Time: 5 ms (40.6%), Space: 29.4 MB (51.5%) - LeetHub

--- 3471-find-the-largest-almost-missing-integer/3471-find-the-largest-almost-missing-integer.cpp
+++ 3471-find-the-largest-almost-missing-integer/3471-find-the-largest-almost-missing-integer.cpp
@@ -4,19 +4,24 @@ public:
         int n=nums.size();
         unordered_map<int,int>mp;
         int maxi=-1;
-        for(int i=0;i<=n-k;i++){
-            unordered_set<int>st;
-            for(int j=0;j<k;j++){
-                st.insert(nums[i+j]);
-            }
-            for(int num:st){
-                mp[num]++;
-            }
+        for(int i=0;i<n;i++){
+            mp[nums[i]]++;
         }
-        for(auto &it:mp){
-            if(it.second==1){
-                maxi=max(maxi,it.first);
+        if(k==n) return *max_element(nums.begin(),nums.end());
+        if(k==1){
+            int ans=-1;
+            for(int i=0;i<n;i++){
+                if(mp[nums[i]]==1){
+                ans=max(ans,nums[i]);
+                }
             }
+            return ans;
+        }
+        if(mp[nums[0]]==1){
+            maxi=max(maxi,nums[0]);
+        }
+        if(mp[nums[n-1]]==1){
+            maxi=max(maxi,nums[n-1]);
         }
         return maxi;
     }
